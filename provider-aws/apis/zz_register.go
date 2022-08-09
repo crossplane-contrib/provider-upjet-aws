9ecc01faaa1a1c568dc9aa261b35b41ec2ee132a
ezgidemirel <ezgidemirel91@gmail.com>
2022-08-09 12:14:30 +0300

    Configure Cloudsearch resources
    Signed-off-by: ezgidemirel <ezgidemirel91@gmail.com>

--- provider-aws/apis/zz_register.go
+++ provider-aws/apis/zz_register.go
@@ -17,6 +17,7 @@ import (
 	v1beta1autoscaling "github.com/upbound/official-providers/provider-aws/apis/autoscaling/v1beta1"
 	v1beta1backup "github.com/upbound/official-providers/provider-aws/apis/backup/v1beta1"
 	v1beta1cloudfront "github.com/upbound/official-providers/provider-aws/apis/cloudfront/v1beta1"
+	v1beta1cloudsearch "github.com/upbound/official-providers/provider-aws/apis/cloudsearch/v1beta1"
 	v1beta1cloudwatchlogs "github.com/upbound/official-providers/provider-aws/apis/cloudwatchlogs/v1beta1"
 	v1beta1cognitoidentity "github.com/upbound/official-providers/provider-aws/apis/cognitoidentity/v1beta1"
 	v1beta1cognitoidp "github.com/upbound/official-providers/provider-aws/apis/cognitoidp/v1beta1"
@@ -79,6 +80,7 @@ func init() {
 		v1beta1autoscaling.SchemeBuilder.AddToScheme,
 		v1beta1backup.SchemeBuilder.AddToScheme,
 		v1beta1cloudfront.SchemeBuilder.AddToScheme,
+		v1beta1cloudsearch.SchemeBuilder.AddToScheme,
 		v1beta1cloudwatchlogs.SchemeBuilder.AddToScheme,
 		v1beta1cognitoidentity.SchemeBuilder.AddToScheme,
 		v1beta1cognitoidp.SchemeBuilder.AddToScheme,
