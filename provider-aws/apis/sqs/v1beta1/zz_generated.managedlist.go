0760d0618fd2a91e0d99f5ea30330fdacf1caf13
ezgidemirel <ezgidemirel91@gmail.com>
2022-08-16 16:39:35 +0300

    Configure SQS resources
    Signed-off-by: ezgidemirel <ezgidemirel91@gmail.com>

--- provider-aws/apis/sqs/v1beta1/zz_generated.managedlist.go
+++ provider-aws/apis/sqs/v1beta1/zz_generated.managedlist.go
@@ -15,3 +15,12 @@ func (l *QueueList) GetItems() []resource.Managed {
 	}
 	return items
 }
+
+// GetItems of this QueuePolicyList.
+func (l *QueuePolicyList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
--- provider-aws/apis/sqs/v1beta1/zz_generated_terraformed.go
+++ provider-aws/apis/sqs/v1beta1/zz_generated_terraformed.go
@@ -77,6 +77,7 @@ func (tr *Queue) LateInitialize(attrs []byte) (bool, error) {
 		return false, errors.Wrap(err, "failed to unmarshal Terraform state parameters for late-initialization")
 	}
 	opts := []resource.GenericLateInitializerOption{resource.WithZeroValueJSONOmitEmptyFilter(resource.CNameWildcard)}
+	opts = append(opts, resource.WithNameFilter("NamePrefix"))
 
 	li := resource.NewGenericLateInitializer(opts...)
 	return li.LateInitialize(&tr.Spec.ForProvider, params)
@@ -85,4 +86,78 @@ func (tr *Queue) LateInitialize(attrs []byte) (bool, error) {
 // GetTerraformSchemaVersion returns the associated Terraform schema version
 func (tr *Queue) GetTerraformSchemaVersion() int {
 	return 0
+}
+
+// GetTerraformResourceType returns Terraform resource type for this QueuePolicy
+func (mg *QueuePolicy) GetTerraformResourceType() string {
+	return "aws_sqs_queue_policy"
+}
+
+// GetConnectionDetailsMapping for this QueuePolicy
+func (tr *QueuePolicy) GetConnectionDetailsMapping() map[string]string {
+	return nil
+}
+
+// GetObservation of this QueuePolicy
+func (tr *QueuePolicy) GetObservation() (map[string]any, error) {
+	o, err := json.TFParser.Marshal(tr.Status.AtProvider)
+	if err != nil {
+		return nil, err
+	}
+	base := map[string]any{}
+	return base, json.TFParser.Unmarshal(o, &base)
+}
+
+// SetObservation for this QueuePolicy
+func (tr *QueuePolicy) SetObservation(obs map[string]any) error {
+	p, err := json.TFParser.Marshal(obs)
+	if err != nil {
+		return err
+	}
+	return json.TFParser.Unmarshal(p, &tr.Status.AtProvider)
+}
+
+// GetID returns ID of underlying Terraform resource of this QueuePolicy
+func (tr *QueuePolicy) GetID() string {
+	if tr.Status.AtProvider.ID == nil {
+		return ""
+	}
+	return *tr.Status.AtProvider.ID
+}
+
+// GetParameters of this QueuePolicy
+func (tr *QueuePolicy) GetParameters() (map[string]any, error) {
+	p, err := json.TFParser.Marshal(tr.Spec.ForProvider)
+	if err != nil {
+		return nil, err
+	}
+	base := map[string]any{}
+	return base, json.TFParser.Unmarshal(p, &base)
+}
+
+// SetParameters for this QueuePolicy
+func (tr *QueuePolicy) SetParameters(params map[string]any) error {
+	p, err := json.TFParser.Marshal(params)
+	if err != nil {
+		return err
+	}
+	return json.TFParser.Unmarshal(p, &tr.Spec.ForProvider)
+}
+
+// LateInitialize this QueuePolicy using its observed tfState.
+// returns True if there are any spec changes for the resource.
+func (tr *QueuePolicy) LateInitialize(attrs []byte) (bool, error) {
+	params := &QueuePolicyParameters{}
+	if err := json.TFParser.Unmarshal(attrs, params); err != nil {
+		return false, errors.Wrap(err, "failed to unmarshal Terraform state parameters for late-initialization")
+	}
+	opts := []resource.GenericLateInitializerOption{resource.WithZeroValueJSONOmitEmptyFilter(resource.CNameWildcard)}
+
+	li := resource.NewGenericLateInitializer(opts...)
+	return li.LateInitialize(&tr.Spec.ForProvider, params)
+}
+
+// GetTerraformSchemaVersion returns the associated Terraform schema version
+func (tr *QueuePolicy) GetTerraformSchemaVersion() int {
+	return 1
 }
