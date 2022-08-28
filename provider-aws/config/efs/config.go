f53c6789a364167b9a20be7345181920c17b3ebe
Muvaffak Onus <me@muvaf.com>
2022-08-28 14:02:58 +0300

    aws.efs: fix examples
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- provider-aws/config/efs/config.go
+++ provider-aws/config/efs/config.go
@@ -5,6 +5,7 @@ import "github.com/upbound/upjet/pkg/config"
 // Configure adds configurations for efs group.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("aws_efs_mount_target", func(r *config.Resource) {
+		r.UseAsync = true
 		r.References["file_system_id"] = config.Reference{
 			Type: "FileSystem",
 		}
