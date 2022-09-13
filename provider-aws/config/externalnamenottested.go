c5ebf0096b9d11e912302d302cb8e718111380cd
Sergen Yalçın <yalcinsergen97@gmail.com>
2022-09-13 19:45:20 +0300

    Add some (40) AWS configurations without testing
    Signed-off-by: Sergen Yalçın <yalcinsergen97@gmail.com>

--- provider-aws/config/externalnamenottested.go
+++ provider-aws/config/externalnamenottested.go
@@ -12,45 +12,45 @@ import (
 // provider.
 var ExternalNameNotTestedConfigs = map[string]config.ExternalName{
 	// accessanalyzer
-
+	//
 	// Access Analyzer Analyzers can be imported using the analyzer_name
 	"aws_accessanalyzer_analyzer": config.ParameterAsIdentifier("analyzer_name"),
 
 	// account
-
-	// the Alternate Contact can be imported using the account_id and alternate_contact_type separated by a forward slash (/)
-	"aws_account_alternate_contact": FormattedIdentifierUserDefined("/", "account_id", "alternate_contact_type"),
+	//
+	// The Alternate Contact for the current account can be imported using the alternate_contact_type
+	"aws_account_alternate_contact": config.TemplatedStringAsIdentifier("", "{{ .parameters.alternate_contact_type }}"),
 
 	// amp
-
+	//
 	// The prometheus alert manager definition can be imported using the workspace identifier
-	"aws_prometheus_alert_manager_definition": config.IdentifierFromProvider,
+	"aws_prometheus_alert_manager_definition": config.ParameterAsIdentifier("workspace_id"),
 	// The prometheus rule group namespace can be imported using the arn
 	"aws_prometheus_rule_group_namespace": config.IdentifierFromProvider,
 	// AMP Workspaces can be imported using the identifier
 	"aws_prometheus_workspace": config.IdentifierFromProvider,
 
 	// amplify
-
+	//
 	// Amplify App can be imported using Amplify App ID (appId)
 	"aws_amplify_app": config.IdentifierFromProvider,
-	// Amplify backend environment can be imported using app_id and environment_name
-	"aws_amplify_backend_environment": FormattedIdentifierUserDefined("/", "app_id", "environment_name"),
-	// Amplify branch can be imported using app_id and branch_name
-	"aws_amplify_branch": FormattedIdentifierUserDefined("/", "app_id", "branch_name"),
-	// Amplify domain association can be imported using app_id and domain_name
-	"aws_amplify_domain_association": FormattedIdentifierUserDefined("/", "app_id", "domain_name"),
+	// Amplify backend environment can be imported using app_id and environment_name: d2ypk4k47z8u6/example
+	"aws_amplify_backend_environment": config.TemplatedStringAsIdentifier("environment_name", "{{ .parameters.app_id }}/{{ .external_name }}"),
+	// Amplify branch can be imported using app_id and branch_name: d2ypk4k47z8u6/master
+	"aws_amplify_branch": config.TemplatedStringAsIdentifier("branch_name", "{{ .parameters.app_id }}/{{ .external_name }}"),
+	// Amplify domain association can be imported using app_id and domain_name: d2ypk4k47z8u6/example.com
+	"aws_amplify_domain_association": config.TemplatedStringAsIdentifier("domain_name", "{{ .parameters.app_id }}/{{ .external_name }}"),
 	// Amplify webhook can be imported using a webhook ID
 	"aws_amplify_webhook": config.IdentifierFromProvider,
 
 	// appautoscaling
-
-	// Application AutoScaling Policy can be imported using the service-namespace , resource-id, scalable-dimension and policy-name separated by /
-	"aws_appautoscaling_policy": FormattedIdentifierUserDefined("/", "service_namespace", "resource_id", "scalable_dimension", "name"),
+	//
+	// Application AutoScaling Policy can be imported using the service-namespace, resource-id, scalable-dimension and policy-name separated by /
+	"aws_appautoscaling_policy": config.TemplatedStringAsIdentifier("name", "{{ .parameters.service_namespace }}/{{ .parameters.resource_id }}/{{ .parameters.scalable_dimension }}/{{ .external_name }}"),
 	// No import
 	"aws_appautoscaling_scheduled_action": config.IdentifierFromProvider,
 	// Application AutoScaling Target can be imported using the service-namespace , resource-id and scalable-dimension separated by /
-	"aws_appautoscaling_target": FormattedIdentifierUserDefined("/", "service_namespace", "resource_id", "scalable_dimension"),
+	"aws_appautoscaling_target": config.TemplatedStringAsIdentifier("", "{{ .parameters.service_namespace }}/{{ .parameters.resource_id }}/{{ .parameters.scalable_dimension }}"),
 
 	// appconfig
 
@@ -66,6 +66,113 @@ var ExternalNameNotTestedConfigs = map[string]config.ExternalName{
 	"aws_appconfig_environment": config.IdentifierFromProvider,
 	// AppConfig Hosted Configuration Versions can be imported by using the application ID, configuration profile ID, and version number separated by a slash (/)
 	"aws_appconfig_hosted_configuration_version": config.IdentifierFromProvider,
+
+	// appflow
+	//
 	// AppFlow flows can be imported using the arn
 	"aws_appflow_flow": config.IdentifierFromProvider,
+
+	// appintegrations
+	//
+	// Amazon AppIntegrations Event Integrations can be imported using the name
+	"aws_appintegrations_event_integration": config.NameAsIdentifier,
+
+	// appmesh
+	//
+	// App Mesh gateway routes can be imported using mesh_name and virtual_gateway_name together with the gateway route's name, e.g.,
+	// mesh/gw1/example-gateway-route
+	"aws_appmesh_gateway_route": config.TemplatedStringAsIdentifier("name", "{{ .parameters.mesh_name }}/{{ .parameters.virtual_gateway_name }}/{{ .external_name }}"),
+	// App Mesh service meshes can be imported using the name
+	"aws_appmesh_mesh": config.NameAsIdentifier,
+	// App Mesh virtual routes can be imported using mesh_name and virtual_router_name together with the route's name, e.g.,
+	// simpleapp/serviceB/serviceB-route
+	"aws_appmesh_route": config.TemplatedStringAsIdentifier("name", "{{ .parameters.mesh_name }}/{{ .parameters.virtual_router_name }}/{{ .external_name }}"),
+	// App Mesh virtual gateway can be imported using mesh_name together with the virtual gateway's name: mesh/gw1
+	"aws_appmesh_virtual_gateway": config.TemplatedStringAsIdentifier("name", "{{ .parameters.mesh_name }}/{{ .external_name }}"),
+	// App Mesh virtual nodes can be imported using mesh_name together with the virtual node's name: simpleapp/serviceBv1
+	"aws_appmesh_virtual_node": config.TemplatedStringAsIdentifier("name", "{{ .parameters.mesh_name }}/{{ .external_name }}"),
+	// App Mesh virtual routers can be imported using mesh_name together with the virtual router's name: simpleapp/serviceB
+	"aws_appmesh_virtual_router": config.TemplatedStringAsIdentifier("name", "{{ .parameters.mesh_name }}/{{ .external_name }}"),
+	// App Mesh virtual services can be imported using mesh_name together with the virtual service's name: simpleapp/servicea.simpleapp.local
+	"aws_appmesh_virtual_service": config.TemplatedStringAsIdentifier("name", "{{ .parameters.mesh_name }}/{{ .external_name }}"),
+
+	// apprunner
+	//
+	// App Runner AutoScaling Configuration Versions can be imported by using the arn
+	"aws_apprunner_auto_scaling_configuration_version": config.IdentifierFromProvider,
+	// App Runner Connections can be imported by using the connection_name
+	"aws_apprunner_connection": config.ParameterAsIdentifier("connection_name"),
+	// App Runner Custom Domain Associations can be imported by using the domain_name and service_arn separated by a comma (,)
+	"aws_apprunner_custom_domain_association": config.TemplatedStringAsIdentifier("domain_name", "{{ .external_name }},{{ .parameters.service_arn }}"),
+	// App Runner Services can be imported by using the arn
+	"aws_apprunner_service": config.IdentifierFromProvider,
+	// App Runner vpc connector can be imported by using the arn
+	"aws_apprunner_vpc_connector": config.IdentifierFromProvider,
+
+	// appstream
+	//
+	// aws_appstream_directory_config can be imported using the id
+	"aws_appstream_directory_config": config.IdentifierFromProvider,
+	// aws_appstream_fleet can be imported using the id
+	"aws_appstream_fleet": config.IdentifierFromProvider,
+	// AppStream Stack Fleet Association can be imported by using the fleet_name and stack_name separated by a slash (/)
+	"aws_appstream_fleet_stack_association": config.TemplatedStringAsIdentifier("stack_name", "{{ .parameters.fleet_name }}/{{ .external_name }}"),
+	// aws_appstream_image_builder can be imported using the name
+	"aws_appstream_image_builder": config.NameAsIdentifier,
+	// aws_appstream_stack can be imported using the id
+	"aws_appstream_stack": config.IdentifierFromProvider,
+	// aws_appstream_user can be imported using the user_name and authentication_type separated by a slash (/)
+	"aws_appstream_user": config.TemplatedStringAsIdentifier("user_name", "{{ .external_name }}/{{ .parameters.authentication_type }}"),
+	// AppStream User Stack Association can be imported by using the user_name, authentication_type, and stack_name, separated by a slash (/)
+	"aws_appstream_user_stack_association": config.TemplatedStringAsIdentifier("stack_name", "{{ .parameters.user_name }}/{{ .parameters.authentication_type }}/{{ .external_name }}/"),
+
+	// appsync
+	//
+	// aws_appsync_api_cache can be imported using the AppSync API ID
+	"aws_appsync_api_cache": config.IdentifierFromProvider,
+	// aws_appsync_api_key can be imported using the AppSync API ID and key separated by :
+	"aws_appsync_api_key": config.IdentifierFromProvider,
+	// aws_appsync_datasource can be imported with their api_id, a hyphen, and name
+	"aws_appsync_datasource": config.TemplatedStringAsIdentifier("name", "{{ .parameters.api_id }}-{{ .external_name }}"),
+	// aws_appsync_domain_name can be imported using the AppSync domain name
+	"aws_appsync_domain_name": config.ParameterAsIdentifier("domain_name"),
+	// aws_appsync_domain_name_api_association can be imported using the AppSync domain name
+	"aws_appsync_domain_name_api_association": config.ParameterAsIdentifier("domain_name"),
+	// aws_appsync_function can be imported using the AppSync API ID and Function ID separated by -
+	"aws_appsync_function": config.IdentifierFromProvider,
+	// AppSync GraphQL API can be imported using the GraphQL API ID
+	"aws_appsync_graphql_api": config.IdentifierFromProvider,
+	// aws_appsync_resolver can be imported with their api_id, a hyphen, type, a hypen and field
+	"aws_appsync_resolver": config.TemplatedStringAsIdentifier("", "{{ .parameters.api_id }}-{{ .parameters.type }}-{{ .parameters.field }}"),
+
+	// autoscaling
+	//
+	// aws_autoscaling_group_tag can be imported by using the ASG name and key, separated by a comma (,)
+	"aws_autoscaling_group_tag": config.TemplatedStringAsIdentifier("autoscaling_group_name", "{{ .external_name }},{{ .parameters.tag.key }}"),
+	// AutoScaling Lifecycle Hooks can be imported using the role autoscaling_group_name and name separated by /
+	"aws_autoscaling_lifecycle_hook": config.TemplatedStringAsIdentifier("name", "{{ .parameters.autoscaling_group_name }}/{{ .external_name }}"),
+	// No import
+	"aws_autoscaling_notification": config.IdentifierFromProvider,
+	// AutoScaling scaling policy can be imported using the role autoscaling_group_name and name separated by /
+	"aws_autoscaling_policy": config.TemplatedStringAsIdentifier("name", "{{ .parameters.autoscaling_group_name }}/{{ .external_name }}"),
+	// AutoScaling ScheduledAction can be imported using the auto-scaling-group-name and scheduled-action-name: auto-scaling-group-name/scheduled-action-name
+	"aws_autoscaling_schedule": config.TemplatedStringAsIdentifier("scheduled_action_name", "{{ .parameters.autoscaling_group_name }}/{{ .external_name }}"),
+	// Launch configurations can be imported using the name
+	"aws_launch_configuration": config.NameAsIdentifier,
+
+	// autoscalingplans
+	//
+	// Auto Scaling scaling plans can be imported using the name
+	"aws_autoscalingplans_scaling_plan": config.NameAsIdentifier,
+
+	// batch
+	//
+	// AWS Batch compute can be imported using the compute_environment_name
+	"aws_batch_compute_environment": config.ParameterAsIdentifier("compute_environment_name"),
+	// Batch Job Definition can be imported using the arn: arn:aws:batch:us-east-1:123456789012:job-definition/sample
+	"aws_batch_job_definition": config.TemplatedStringAsIdentifier("name", "arn:aws:batch:{{ .setup.configuration.region }}:{{ .setup.client_metadata.account_id }}:job-definition/{{ .external_name }}"),
+	// Batch Job Queue can be imported using the arn: arn:aws:batch:us-east-1:123456789012:job-queue/sample
+	"aws_batch_job_queue": config.TemplatedStringAsIdentifier("name", "arn:aws:batch:{{ .setup.configuration.region }}:{{ .setup.client_metadata.account_id }}:job-queue/{{ .external_name }}"),
+	// Batch Scheduling Policy can be imported using the arn: arn:aws:batch:us-east-1:123456789012:scheduling-policy/sample
+	"aws_batch_scheduling_policy": config.TemplatedStringAsIdentifier("name", "arn:aws:batch:{{ .setup.configuration.region }}:{{ .setup.client_metadata.account_id }}:scheduling-policy/{{ .external_name }}"),
 }
