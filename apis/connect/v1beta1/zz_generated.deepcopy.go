60961216ddc4e8d0a9abea4467ef6e1457efa57a
Fatih Türken <turkenf@gmail.com>
2022-11-02 21:48:58 +0300

    Add reference config and fix example for aws_connect_quick_connect

--- apis/connect/v1beta1/zz_generated.deepcopy.go
+++ apis/connect/v1beta1/zz_generated.deepcopy.go
@@ -1860,6 +1860,16 @@ func (in *QuickConnectParameters) DeepCopyInto(out *QuickConnectParameters) {
 		*out = new(string)
 		**out = **in
 	}
+	if in.InstanceIDRef != nil {
+		in, out := &in.InstanceIDRef, &out.InstanceIDRef
+		*out = new(v1.Reference)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.InstanceIDSelector != nil {
+		in, out := &in.InstanceIDSelector, &out.InstanceIDSelector
+		*out = new(v1.Selector)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.Name != nil {
 		in, out := &in.Name, &out.Name
 		*out = new(string)
