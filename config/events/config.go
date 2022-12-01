4f8f247e100eeaf15ab0145c908f7e56ff3082da
Aditya Menon <106598482+zeresius@users.noreply.github.com>
2022-12-01 22:04:46 +0100

    Simplify reference to Bus
    Co-authored-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- config/events/config.go
+++ config/events/config.go
@@ -28,20 +28,14 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("aws_cloudwatch_event_rule", func(r *config.Resource) {
 		r.References["event_bus_name"] = config.Reference{
-			Type:              "github.com/upbound/provider-aws/apis/events/v1beta1.Bus",
-			RefFieldName:      "EventBusNameRefs",
-			SelectorFieldName: "EventBusNameSelector",
+			Type:              "Bus",
 		}
-		r.UseAsync = true
 	})
 	p.AddResourceConfigurator("aws_cloudwatch_event_target", func(r *config.Resource) {
 		r.References["event_bus_name"] = config.Reference{
-			Type:              "github.com/upbound/provider-aws/apis/events/v1beta1.Bus",
-			RefFieldName:      "EventBusNameRefs",
-			SelectorFieldName: "EventBusNameSelector",
+			Type:              "Bus",
 		}
 		delete(r.References, "arn")
-		r.UseAsync = true
 	})
 	p.AddResourceConfigurator("aws_cloudwatch_event_permission", func(r *config.Resource) {
 		r.UseAsync = true
