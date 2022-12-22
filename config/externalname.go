f13e09ee6ae0eb9f3e1ea6adbb3ef4d05697ccfb
kdibrov <kdibrov@gmail.com>
2022-12-22 14:36:56 +0100

    Added 10 resources for sagemaker group

--- config/externalname.go
+++ config/externalname.go
@@ -1764,6 +1764,28 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	//
 	// EMR Security Configurations can be imported using the name
 	"aws_emr_security_configuration": config.NameAsIdentifier,
+	// sagemaker
+	//
+	// SageMaker App Image Configs can be imported using the name
+	"aws_sagemaker_app_image_config": config.ParameterAsIdentifier("app_image_config_name"),
+	// SageMaker Code Repositories can be imported using the name
+	"aws_sagemaker_code_repository": config.ParameterAsIdentifier("code_repository_name"),
+	// SageMaker Domains can be imported using the id
+	"aws_sagemaker_domain": config.IdentifierFromProvider,
+	// Feature Groups can be imported using the name
+	"aws_sagemaker_feature_group": config.ParameterAsIdentifier("feature_group_name"),
+	// SageMaker Code Images can be imported using the name
+	"aws_sagemaker_image": config.ParameterAsIdentifier("image_name"),
+	// SageMaker Model Package Groups can be imported using the name
+	"aws_sagemaker_model_package_group": config.ParameterAsIdentifier("model_package_group_name"),
+	// SageMaker Notebook Instances can be imported using the name
+	"aws_sagemaker_notebook_instance": config.NameAsIdentifier,
+	// Models can be imported using the name
+	"aws_sagemaker_notebook_instance_lifecycle_configuration": config.NameAsIdentifier,
+	// SageMaker Studio Lifecycle Configs can be imported using the studio_lifecycle_config_name
+	"aws_sagemaker_studio_lifecycle_config": config.ParameterAsIdentifier("studio_lifecycle_config_name"),
+	// SageMaker User Profiles can be imported using the arn
+	"aws_sagemaker_user_profile": config.IdentifierFromProvider,
 }
 
 func lambdaFunctionURL() config.ExternalName {
