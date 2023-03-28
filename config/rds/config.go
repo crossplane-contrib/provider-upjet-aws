8f71a45fcd605d9afe70cd5cab3bd442a35c0235
Muvaffak Onus <me@muvaf.com>
2023-03-28 03:03:15 +0300

    rds.instance: add unit tests for the password generator
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- config/rds/config.go
+++ config/rds/config.go
@@ -10,12 +10,12 @@ import (
 	"github.com/upbound/upjet/pkg/types/comments"
 
 	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
 	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
 	"github.com/crossplane/crossplane-runtime/pkg/password"
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -23,6 +23,10 @@ import (
 	"github.com/upbound/upjet/pkg/config"
 
 	"github.com/upbound/provider-aws/config/common"
+)
+
+const (
+	errGetPasswordSecret = "cannot get password secret"
 )
 
 // Configure adds configurations for rds group.
@@ -110,7 +114,9 @@ func Configure(p *config.Provider) {
 			if a, ok := attr["port"]; ok {
 				conn["port"] = []byte(fmt.Sprintf("%v", a))
 			}
-
+			if a, ok := attr["password"].(string); ok {
+				conn["password"] = []byte(a)
+			}
 			return conn, nil
 		}
 		desc, _ := comments.New("If true, the password will be auto-generated and"+
@@ -121,48 +127,15 @@ func Configure(p *config.Provider) {
 			Optional:    true,
 			Description: desc.String(),
 		}
-		r.InitializerFns = append(r.InitializerFns, func(client client.Client) managed.Initializer {
-			return managed.InitializerFn(func(ctx context.Context, mg resource.Managed) error {
-				paved, err := fieldpath.PaveObject(mg)
-				if err != nil {
-					return err
-				}
-				sel := &v1.SecretKeySelector{}
-				if err := paved.GetValueInto("spec.forProvider.passwordSecretRef", sel); err != nil {
-					return errors.Wrap(err, "cannot unmarshal passwordSecretRef into a secret key selector")
-				}
-				s := &corev1.Secret{}
-				if err := client.Get(ctx, types.NamespacedName{Namespace: sel.Namespace, Name: sel.Name}, s); resource.IgnoreNotFound(err) != nil {
-					return errors.Wrap(err, "cannot get password secret")
-				}
-				if err == nil && len(s.Data[sel.Key]) != 0 {
-					// Password is already set.
-					return nil
-				}
-				// At this point, either the secret doesn't exist, or it doesn't
-				// have the password filled.
-				gen, err := paved.GetBool("spec.forProvider.autoGeneratePassword")
-				if err != nil {
-					return errors.Wrap(err, "cannot get autoGeneratePassword field value")
-				}
-				if !gen {
-					// Password is not set, and we don't want to generate one.
-					return nil
-				}
-				pw, err := password.Generate()
-				if err != nil {
-					return errors.Wrap(err, "cannot generate password")
-				}
-				s.SetName(sel.Name)
-				s.SetNamespace(sel.Namespace)
-				if s.Data == nil {
-					s.Data = make(map[string][]byte, 1)
-				}
-				s.Data[sel.Key] = []byte(pw)
-				return errors.Wrap(resource.NewAPIPatchingApplicator(client).Apply(ctx, s), "cannot apply password secret")
-			})
-		})
-		r.TerraformResource.Schema["password"].Description = "Password for the master DB user. If you set autoGeneratePassword to true, the Secret referenced here will be created or updated with generated password if it does not already contain one."
+		r.InitializerFns = append(r.InitializerFns,
+			PasswordGenerator(
+				"spec.forProvider.passwordSecretRef",
+				"spec.forProvider.autoGeneratePassword",
+			))
+		r.TerraformResource.Schema["password"].Description = "Password for the " +
+			"master DB user. If you set autoGeneratePassword to true, the Secret" +
+			" referenced here will be created or updated with generated password" +
+			" if it does not already contain one."
 	})
 
 	p.AddResourceConfigurator("aws_db_proxy", func(r *config.Resource) {
@@ -196,4 +169,58 @@ func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("aws_rds_cluster_role_association", func(r *config.Resource) {
 		r.UseAsync = true
 	})
+}
+
+// PasswordGenerator returns an InitializerFn that will generate a password
+// for a resource if the toggle field is set to true and the secret referenced
+// by the secretRefFieldPath is not found or does not have content corresponding
+// to the password key.
+func PasswordGenerator(secretRefFieldPath, toggleFieldPath string) config.NewInitializerFn {
+	return func(client client.Client) managed.Initializer {
+		return managed.InitializerFn(func(ctx context.Context, mg resource.Managed) error {
+			paved, err := fieldpath.PaveObject(mg)
+			if err != nil {
+				return errors.Wrap(err, "cannot pave object")
+			}
+			sel := &v1.SecretKeySelector{}
+			err = paved.GetValueInto(secretRefFieldPath, sel)
+			if fieldpath.IsNotFound(err) {
+				// No secret reference is given, user wants to submit empty
+				// password.
+				return nil
+			}
+			if err != nil {
+				return errors.Wrapf(err, "cannot unmarshal %s into a secret key selector", secretRefFieldPath)
+			}
+			s := &corev1.Secret{}
+			if err := client.Get(ctx, types.NamespacedName{Namespace: sel.Namespace, Name: sel.Name}, s); resource.IgnoreNotFound(err) != nil {
+				return errors.Wrap(err, errGetPasswordSecret)
+			}
+			if err == nil && len(s.Data[sel.Key]) != 0 {
+				// Password is already set.
+				return nil
+			}
+			// At this point, either the secret doesn't exist, or it doesn't
+			// have the password filled.
+			gen, err := paved.GetBool(toggleFieldPath)
+			if resource.Ignore(fieldpath.IsNotFound, err) != nil {
+				return errors.Wrapf(err, "cannot get the value of %s", toggleFieldPath)
+			}
+			if !gen {
+				// Password is not set, and we don't want to generate one.
+				return nil
+			}
+			pw, err := password.Generate()
+			if err != nil {
+				return errors.Wrap(err, "cannot generate password")
+			}
+			s.SetName(sel.Name)
+			s.SetNamespace(sel.Namespace)
+			if s.Data == nil {
+				s.Data = make(map[string][]byte, 1)
+			}
+			s.Data[sel.Key] = []byte(pw)
+			return errors.Wrap(resource.NewAPIPatchingApplicator(client).Apply(ctx, s), "cannot apply password secret")
+		})
+	}
 }
