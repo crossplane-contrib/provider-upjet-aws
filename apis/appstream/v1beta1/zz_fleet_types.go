4252c8bf98a5b1712aa7d65782764161caea3c3c
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2023-04-03 18:12:33 +0300

    Configure plural names for resources whose kind names end with "fleet"
    - Please see: https://github.com/upbound/provider-aws/issues/646
    
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
    (cherry picked from commit de90966c5f67f45693412563f2e84202468f5d63)

--- apis/appstream/v1beta1/zz_fleet_types.go
+++ apis/appstream/v1beta1/zz_fleet_types.go
@@ -198,7 +198,7 @@ type FleetStatus struct {
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,aws}
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,aws},path=fleet
 type Fleet struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
