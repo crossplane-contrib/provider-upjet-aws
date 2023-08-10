cacecf5514b0b873b7fa180ada34ffde41086ba3
Fatih Türken <turkenf@gmail.com>
2023-08-10 20:42:22 +0300

    Add pre-delete-hook to resourceshareaccepter.yaml

---
+++ examples/ram/testhooks/pre-delete-principal.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -aeuo pipefail
+
+# Note(turkenf): We are getting "ResourceShare arn....  could not be found" error  
+# when deleted before the ResourceShare resource ResourceShareAccepter resource. 
+# This is a workaround for this problem to make automated tests to work.
+${KUBECTL} delete resourceshareaccepter.ram.aws.upbound.io -l testing.upbound.io/example-name=example
