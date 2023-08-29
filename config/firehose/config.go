d382cf90a90ca0020813eb166991141a4b1b8aaa
Christopher Haar <christopher.haar@upbound.io>
2023-08-29 15:22:31 +0200

    fix(lateinit): skip version_id for firehose deliverystream
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- config/firehose/config.go
+++ config/firehose/config.go
@@ -36,7 +36,10 @@ func Configure(p *config.Provider) {
 		}
 
 		r.LateInitializer = config.LateInitializer{
-			IgnoredFields: []string{"server_side_encryption"},
+			IgnoredFields: []string{
+				"server_side_encryption",
+				"version_id",
+			},
 		}
 
 		delete(r.References, "extended_s3_configuration.data_format_conversion_configuration.schema_configuration.database_name")
