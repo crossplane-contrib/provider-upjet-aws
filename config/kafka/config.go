b33089cca5e8e861a02f0e21408808294851b5d1
Matt Bush <matt@span.io>
2023-09-24 14:35:31 -0700

    lint

--- config/kafka/config.go
+++ config/kafka/config.go
@@ -33,7 +33,7 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("aws_msk_scram_secret_association", func(r *config.Resource) {
 		r.References["secret_arn_list"] = config.Reference{
-			Type: "github.com/upbound/provider-aws/apis/secretsmanager/v1beta1.Secret",
+			Type:              "github.com/upbound/provider-aws/apis/secretsmanager/v1beta1.Secret",
 			RefFieldName:      "SecretArnRefs",
 			SelectorFieldName: "SecretArnSelector",
 		}
