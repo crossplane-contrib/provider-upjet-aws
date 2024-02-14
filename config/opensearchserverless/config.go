ea95d081ac65436bcbf62dacc3ef7594bb1c1b2f
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2024-02-14 23:17:54 +0300

    Canonicalize the JSON "policy" parameters of {LifecyclePolicy,AccessPolicy,SecurityPolicy}.opensearchserverless resources
    - Update the example manifests examples/opensearchserverless/v1beta1/{lifecyclepolicy,accesspolicy,securitypolicy}.yaml
      The "policy" parameters are now multi-line JSON strings.
    
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- config/opensearchserverless/config.go
+++ config/opensearchserverless/config.go
@@ -4,11 +4,22 @@ Copyright 2024 Upbound Inc.
 
 package opensearchserverless
 
-import "github.com/crossplane/upjet/pkg/config"
+import (
+	"github.com/crossplane/upjet/pkg/config"
+)
 
 // Configure adds configurations for the opensearchserverless group.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("aws_opensearchserverless_security_config", func(r *config.Resource) {
 		r.SchemaElementOptions.SetEmbeddedObject("saml_options")
+	})
+	p.AddResourceConfigurator("aws_opensearchserverless_security_policy", func(r *config.Resource) {
+		r.TerraformConfigurationInjector = config.CanonicalizeJSONParameters("policy")
+	})
+	p.AddResourceConfigurator("aws_opensearchserverless_lifecycle_policy", func(r *config.Resource) {
+		r.TerraformConfigurationInjector = config.CanonicalizeJSONParameters("policy")
+	})
+	p.AddResourceConfigurator("aws_opensearchserverless_access_policy", func(r *config.Resource) {
+		r.TerraformConfigurationInjector = config.CanonicalizeJSONParameters("policy")
 	})
 }
