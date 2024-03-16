4b267748ca253fb03d4910a5e418b6a63c9b16fb
Matt Bush <mbbush@gmail.com>
2024-03-16 00:38:39 -0700

    Add examples for many different types of iot topic rule
    Signed-off-by: Matt Bush <mbbush@gmail.com>

--- examples/iot/v1beta1/testhooks/delete-topicrule-kafka.sh
+++ examples/iot/v1beta1/testhooks/delete-topicrule-kafka.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -aeuo pipefail
 
-# SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
-#
-# SPDX-License-Identifier: CC0-1.0
-
 # Note(mbbush): AWS IoT uses the IAM role assigned to the TopicRuleDestination during deletion
 # of the TopicRuleDestination to remove the ENIs it created in the VPC.
 # Deletion of the TopicRuleDestination takes several minutes, and if the role is deleted before
@@ -12,4 +8,4 @@ set -aeuo pipefail
 # because there are still resources left in it. This ordered deletion requirement could be
 # encoded in a crossplane Usage resource, but that's still alpha and not readily available
 # in the current uptest config. This also solves the problem.
-${KUBECTL} delete topicruledestination.iot.aws.upbound.io/iot-topic-rule-destination-example
+${KUBECTL} delete topicruledestination.iot.aws.upbound.io/topicrule-kafka
