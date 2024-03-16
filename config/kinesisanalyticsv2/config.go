fe3ef18c657ccaad9309c552df2f672d809b157b
Matt Bush <mbbush@gmail.com>
2024-03-15 23:50:35 -0700

    Add references for subnet and security group to Application.kinesisanalyticsv2
    Signed-off-by: Matt Bush <mbbush@gmail.com>

--- config/kinesisanalyticsv2/config.go
+++ config/kinesisanalyticsv2/config.go
@@ -13,20 +13,34 @@ import (
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("aws_kinesisanalyticsv2_application", func(r *config.Resource) {
 		r.References["application_configuration.application_code_configuration.code_content.s3_content_location.bucket_arn"] = config.Reference{
-			Type:      "github.com/upbound/provider-aws/apis/s3/v1beta1.Bucket",
-			Extractor: common.PathARNExtractor,
+			TerraformName: "aws_s3_bucket",
+			Extractor:     common.PathARNExtractor,
 		}
 		r.References["service_execution_role"] = config.Reference{
-			Type:      "github.com/upbound/provider-aws/apis/iam/v1beta1.Role",
-			Extractor: common.PathARNExtractor,
+			TerraformName: "aws_iam_role",
+			Extractor:     common.PathARNExtractor,
 		}
 		r.References["application_configuration.sql_application_configuration.reference_data_source.s3_reference_data_source.bucket_arn"] = config.Reference{
-			Type:      "github.com/upbound/provider-aws/apis/s3/v1beta1.Bucket",
-			Extractor: `github.com/crossplane/upjet/pkg/resource.ExtractParamPath("arn",true)`,
+			TerraformName: "aws_s3_bucket",
+			Extractor:     common.PathARNExtractor,
 		}
 		r.References["application_configuration.sql_application_configuration.input.kinesis_streams_input.resource_arn"] = config.Reference{
 			TerraformName: "aws_kinesis_stream",
 			Extractor:     common.PathTerraformIDExtractor,
+		}
+		r.References["application_configuration.vpc_configuration.security_group_ids"] = config.Reference{
+			TerraformName:     "aws_security_group",
+			RefFieldName:      "SecurityGroupIDRefs",
+			SelectorFieldName: "SecurityGroupIDSelector",
+		}
+		r.References["application_configuration.vpc_configuration.subnet_ids"] = config.Reference{
+			TerraformName:     "aws_subnet",
+			RefFieldName:      "SubnetIDRefs",
+			SelectorFieldName: "SubnetIDSelector",
+		}
+		r.References["application_configuration.sql_application_configuration.input.input_processing_configuration.input_lambda_processor.resource_arn"] = config.Reference{
+			TerraformName: "aws_lambda_function",
+			Extractor:     common.PathARNExtractor,
 		}
 	})
 }
