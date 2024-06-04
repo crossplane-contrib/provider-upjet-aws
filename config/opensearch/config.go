4ee826c1d9de88a744c02a4ac2997e9bd8cc79b0
Christopher Haar <christopher.haar@upbound.io>
2024-06-04 14:53:40 +0200

    feat(opensearch): add ref/selector for opensearch domains
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- config/opensearch/config.go
+++ config/opensearch/config.go
@@ -4,12 +4,34 @@
 
 package opensearch
 
-import "github.com/crossplane/upjet/pkg/config"
+import (
+	"github.com/crossplane/upjet/pkg/config"
+
+	"github.com/upbound/provider-aws/config/common"
+)
 
 // Configure adds configurations for the opensearch group.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("aws_opensearch_domain", func(r *config.Resource) {
 		config.MoveToStatus(r.TerraformResource, "access_policies")
+		r.References["encrypt_at_rest.kms_key_id"] = config.Reference{
+			// its KMS key ARN in AWS API
+			TerraformName: "aws_kms_key",
+			Extractor:     common.PathARNExtractor,
+		}
+
+		r.References["vpc_options.security_group_ids"] = config.Reference{
+			TerraformName:     "aws_security_group",
+			RefFieldName:      "SecurityGroupIDRefs",
+			SelectorFieldName: "SecurityGroupIDSelector",
+		}
+
+		r.References["vpc_options.subnet_ids"] = config.Reference{
+			TerraformName:     "aws_subnet",
+			RefFieldName:      "SubnetIDRefs",
+			SelectorFieldName: "SubnetIDSelector",
+		}
+
 		r.UseAsync = true
 	})
 
