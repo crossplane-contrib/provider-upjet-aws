b6d3440ee10f0ce81ae064ac859b73ac14ff7d9b
Cem Mergenci <cmergenci@gmail.com>
2024-11-21 14:33:30 +0300

    Manually backport new `omitempty` struct tags.
    Signed-off-by: Cem Mergenci <cmergenci@gmail.com>
    Signed-off-by: Nick Atzert <nick.atzert@upbound.io>

--- apis/kinesisanalyticsv2/v1beta1/zz_application_types.go
+++ apis/kinesisanalyticsv2/v1beta1/zz_application_types.go
@@ -1694,7 +1694,7 @@ type VPCConfigurationParameters struct {
 	// +crossplane:generate:reference:extractor=github.com/crossplane/upjet/pkg/resource.ExtractResourceID()
 	// +kubebuilder:validation:Optional
 	// +listType=set
-	SecurityGroupIds []*string `json:"securityGroupIds" tf:"security_group_ids,omitempty"`
+	SecurityGroupIds []*string `json:"securityGroupIds,omitempty" tf:"security_group_ids,omitempty"`
 
 	// References to SecurityGroup in ec2 to populate securityGroupIds.
 	// +kubebuilder:validation:Optional
@@ -1709,7 +1709,7 @@ type VPCConfigurationParameters struct {
 	// +crossplane:generate:reference:extractor=github.com/crossplane/upjet/pkg/resource.ExtractResourceID()
 	// +kubebuilder:validation:Optional
 	// +listType=set
-	SubnetIds []*string `json:"subnetIds" tf:"subnet_ids,omitempty"`
+	SubnetIds []*string `json:"subnetIds,omitempty" tf:"subnet_ids,omitempty"`
 
 	// References to Subnet in ec2 to populate subnetIds.
 	// +kubebuilder:validation:Optional
