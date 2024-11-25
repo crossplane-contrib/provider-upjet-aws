ce034b303c563bc110827640ef4ac41a0e31de23
Tim Birkett <tim.birkett@portswigger.net>
2024-11-25 16:33:05 +0000

    feat(elasticache/serverless): fix reader_endpoint name
    Signed-off-by: Tim Birkett <tim.birkett@portswigger.net>

--- config/elasticache/config.go
+++ config/elasticache/config.go
@@ -205,9 +205,15 @@ func Configure(p *config.Provider) { //nolint:gocyclo
 							key := fmt.Sprintf("reader_endpoint_%d_address", i)
 							conn[key] = []byte(address)
 						}
+<<<<<<< HEAD
 						if port, ok := readerendpoint["port"]; ok {
 							key := fmt.Sprintf("reader_endpoint_%d_port", i)
 							conn[key] = []byte(fmt.Sprintf("%v", port))
+=======
+						if port, ok := readerendpoint["port"].(float64); ok {
+							key := fmt.Sprintf("reader_endpoint_%d_port", i)
+							conn[key] = []byte(fmt.Sprintf("%g", port))
+>>>>>>> 8e83741cc (feat(elasticache/serverless): fix reader_endpoint name)
 						}
 					}
 				}
