66fc564f35a65b871e1e7d0f1121bb937b7f1af7
Sergen Yalçın <yalcinsergen97@gmail.com>
2025-02-06 17:20:24 +0300

    Add Update Prevent function for preventing the update loop
    Signed-off-by: Sergen Yalçın <yalcinsergen97@gmail.com>

--- config/mq/config.go
+++ config/mq/config.go
@@ -5,10 +5,15 @@
 package mq
 
 import (
+	"encoding/base64"
 	"fmt"
 
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
+	xpresource "github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/crossplane/upjet/pkg/config"
 	"github.com/crossplane/upjet/pkg/registry"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 // Configure adds configurations for the mq group.
@@ -74,4 +79,66 @@ func Configure(p *config.Provider) { //nolint:gocyclo
 		}
 		r.MetaResource.ArgumentDocs["console_access"] = `- (Optional) Setting consoleAccess will result in an update loop till the MQ Broker to which this user belongs is restarted.`
 	})
+
+	p.AddResourceConfigurator("aws_mq_configuration", func(r *config.Resource) {
+		c := &repeatedDiffCheckerForData{}
+		r.UpdateLoopPrevention = c
+	})
+}
+
+// repeatedDiffCheckerForData implements the UpdateLoopPrevention interface.
+// It is responsible for checking if the same diff related to the "data"
+// argument (in this case, XML content) appears repeatedly, which may indicate
+// a schema violation or invalid data scenario. If a repeated diff is detected,
+// it blocks the update process.
+type repeatedDiffCheckerForData struct {
+	// previousDiff stores the base64-encoded string of the last diff.
+	// This is used to compare against the current diff to detect repeated
+	// updates.
+	previousDiff *string
+}
+
+// UpdateLoopPreventionFunc checks for repeated diffs in the resource's "data"
+// attribute. If the diff has not changed since the previous reconciliation
+// loop, it blocks the update by returning an appropriate result.
+func (c *repeatedDiffCheckerForData) UpdateLoopPreventionFunc(diff *terraform.InstanceDiff, mg xpresource.Managed) (*config.UpdateLoopPreventResult, error) {
+	// Skip processing if there is no diff, the diff is empty, or it is a destroy operation.
+	if diff == nil || diff.Empty() || diff.Destroy || diff.Attributes == nil {
+		return nil, nil
+	}
+	paved, err := fieldpath.PaveObject(mg)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot pave object")
+	}
+	// Retrieve the value of the "spec.forProvider.engineType" field from the paved object.
+	// This field is used to determine the engine type of the MQ configuration.
+	engineType, err := paved.GetString("spec.forProvider.engineType")
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot get value of spec.forProvider.engineType")
+	}
+	// Check if the engine type is "ActiveMQ". If it is not, skip further checks and return nil.
+	// This block ensures that the diff check logic only applies to resources with ActiveMQ engine type,
+	// avoiding unnecessary diff processing for other engine types.
+	if engineType != "ActiveMQ" {
+		return nil, nil
+	}
+	// Encode the "data" attribute of the diff into a base64 string for comparison.
+	var encodedDiff string
+	if dataDiff, ok := diff.Attributes["data"]; ok {
+		// Use GoString to get a string representation of the attribute.
+		encodedDiff = base64.StdEncoding.EncodeToString([]byte(dataDiff.GoString()))
+	}
+	// If there is no previous diff recorded, store the current diff and allow the update to proceed.
+	if c.previousDiff == nil {
+		c.previousDiff = &encodedDiff
+		return nil, nil
+	}
+	// If the current diff matches the previous diff, block the update and return a reason.
+	if encodedDiff == *c.previousDiff {
+		return &config.UpdateLoopPreventResult{Reason: "Repeated diff for the provided XML data, please check the XML content you have provided. " +
+			"It may contain invalid or schema violating content."}, nil
+	}
+	// Update the previous diff with the current diff for the next reconciliation loop.
+	c.previousDiff = &encodedDiff
+	return nil, nil
 }
