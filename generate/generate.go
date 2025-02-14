91f62c69692d5b93fab1e7dae7e98e56d1f5b2ca
Nic Cope <nicc@rk0n.org>
2025-02-13 19:37:37 -0800

    WIP: Generate namespaced types and controllers
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- generate/generate.go
+++ generate/generate.go
@@ -10,12 +10,14 @@
 
 // Remove existing CRDs
 //go:generate rm -rf ../package/crds
+//go:generate rm -rf ../package/crds-namespaced
 
 // Remove generated files
 //go:generate bash -c "find ../apis \\( -iname 'zz_generated.conversion_hubs.go' -o -iname 'zz_generated.conversion_spokes.go' \\) -delete"
 //go:generate bash -c "find ../apis -type d -empty -delete"
+//go:generate bash -c "find ../apis-namespaced \\( -iname 'zz_generated.conversion_hubs.go' -o -iname 'zz_generated.conversion_spokes.go' \\) -delete"
 //go:generate bash -c "find ../internal/controller -iname 'zz_*' -delete"
-//go:generate bash -c "find ../internal/controller -type d -empty -delete"
+//go:generate bash -c "find ../internal/controller -iname 'zz_*' -delete"
 //go:generate bash -c "find ../cmd/provider -name 'zz_*' -type f -delete"
 //go:generate bash -c "find ../cmd/provider -type d -maxdepth 1 -mindepth 1 -empty -delete"
 
@@ -26,17 +28,21 @@
 // that prevent us from using it for generator program.
 
 // Run Terrajet generator
-//go:generate go run ../cmd/generator/main.go ../apis/..
+// TODO(negz): Does it matter that this is only pointed at apis?
+//go:generate go run ../cmd/generator/main.go ..
 
 // Generate deepcopy methodsets and CRD manifests
 //go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=../apis/... crd:allowDangerousTypes=true,crdVersions=v1 output:artifacts:config=../package/crds
+//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=../apis-namespaced/... crd:allowDangerousTypes=true,crdVersions=v1 output:artifacts:config=../package/crds-namespaced
 
 // Generate crossplane-runtime methodsets (resource.Claim, etc)
 //go:generate go run -tags generate github.com/crossplane/crossplane-tools/cmd/angryjet generate-methodsets --header-file=../hack/boilerplate.go.txt ../apis/...
+//go:generate go run -tags generate github.com/crossplane/crossplane-tools/cmd/angryjet generate-methodsets --header-file=../hack/boilerplate.go.txt ../apis-namespaced/...
 
 // Run upjet's transformer for the generated resolvers to get rid of the cross
 // API-group imports and to prevent import cycles
 //go:generate go run github.com/crossplane/upjet/cmd/resolver -g aws.upbound.io -a github.com/upbound/provider-aws/internal/apis -s -p ../apis/...
+//go:generate go run github.com/crossplane/upjet/cmd/resolver -g aws.upbound.io -a github.com/upbound/provider-aws/internal/apis -s -p ../apis-namespaced/...
 
 package apis
 
