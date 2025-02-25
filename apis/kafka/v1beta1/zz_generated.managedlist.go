cee8e69ad8d6b700fc70ea987688d3a13524257c
Fatih Türken <turkenf@gmail.com>
2025-02-26 00:15:28 +0300

    Configure/add Kafka Replicator resource
    Signed-off-by: Fatih Türken <turkenf@gmail.com>

--- apis/kafka/v1beta1/zz_generated.managedlist.go
+++ apis/kafka/v1beta1/zz_generated.managedlist.go
@@ -25,6 +25,15 @@ func (l *ConfigurationList) GetItems() []resource.Managed {
 	return items
 }
 
+// GetItems of this ReplicatorList.
+func (l *ReplicatorList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this ScramSecretAssociationList.
 func (l *ScramSecretAssociationList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
