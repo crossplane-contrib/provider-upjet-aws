e745d91a34f71795fdd7a7e388b2874330fad32a
Fatih Türken <turkenf@gmail.com>
2025-07-09 16:26:04 +0300

    Remove RegionAddition func and add RegionRequired func
    Signed-off-by: Fatih Türken <turkenf@gmail.com>

--- config/overrides.go
+++ config/overrides.go
@@ -8,56 +8,38 @@ import (
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 
 	"github.com/crossplane/upjet/pkg/config"
 	"github.com/crossplane/upjet/pkg/config/conversion"
-	"github.com/crossplane/upjet/pkg/types"
-	"github.com/crossplane/upjet/pkg/types/comments"
 	"github.com/crossplane/upjet/pkg/types/name"
 
 	"github.com/upbound/provider-aws/config/common"
 )
 
-// RegionAddition adds region to the spec of all resources except iam group which
-// does not have a region notion.
-func RegionAddition() config.ResourceOption { //nolint:gocyclo
+// RegionRequired makes the region field required for resources that have it in their schema.
+func RegionRequired() config.ResourceOption { //nolint:gocyclo
 	return func(r *config.Resource) {
-		if r.ShortGroup == "iam" || r.ShortGroup == "opsworks" {
-			return
-		}
-		c := "Region is the region you'd like your resource to be created in.\n"
-		comment, err := comments.New(c, comments.WithTFTag("-"))
-		if err != nil {
-			panic(errors.Wrap(err, "cannot build comment for region"))
-		}
+		// Only process resources that already have a region field
+		if s, ok := r.TerraformResource.Schema["region"]; ok {
+			// Make existing region field required
+			s.Required = true
+			s.Optional = false
+			s.Computed = false
+			s.Description = "Region is the region you'd like your resource to be created in."
 
-		// check if the underlying Terraform resource already has "region"
-		// as a (state) attribute
-		if s, ok := r.TerraformResource.Schema["region"]; ok && types.IsObservation(s) {
-			r.SchemaElementOptions.SetAddToObservation("region")
-		}
-
-		r.TerraformResource.Schema["region"] = &schema.Schema{
-			Type:        schema.TypeString,
-			Required:    true,
-			Description: comment.String(),
-		}
-		if r.MetaResource == nil {
-			return
-		}
-		for _, ex := range r.MetaResource.Examples {
-			defaultRegion := "us-west-1"
-			if err := ex.SetPathValue("region", defaultRegion); err != nil {
-				panic(err)
-			}
-			for k := range ex.Dependencies {
-				if strings.HasPrefix(k, "aws_iam") {
-					continue
-				}
-				if err := ex.Dependencies.SetPathValue(k, "region", defaultRegion); err != nil {
-					panic(err)
+			// Add region to generated examples only if schema has region field
+			if r.MetaResource != nil {
+				for _, ex := range r.MetaResource.Examples {
+					defaultRegion := "us-west-1"
+					if err := ex.SetPathValue("region", defaultRegion); err != nil {
+						panic(err)
+					}
+					for k := range ex.Dependencies {
+						if err := ex.Dependencies.SetPathValue(k, "region", defaultRegion); err != nil {
+							panic(err)
+						}
+					}
 				}
 			}
 		}
