d42f5abc12b8f63ef706362f68dd70f9124d9d00
Erhan Cagirici <erhan@upbound.io>
2026-06-02 15:49:24 +0300

    generate: networkmonitor api group
    Signed-off-by: Erhan Cagirici <erhan@upbound.io>

---
+++ internal/controller/cluster/zz_networkmonitor_setup.go
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplane/upjet/v2/pkg/controller"
+
+	monitor "github.com/upbound/provider-aws/v2/internal/controller/cluster/networkmonitor/monitor"
+	probe "github.com/upbound/provider-aws/v2/internal/controller/cluster/networkmonitor/probe"
+)
+
+// Setup_networkmonitor creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup_networkmonitor(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		monitor.Setup,
+		probe.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupGated_networkmonitor creates all controllers with the supplied logger and adds them to
+// the supplied manager gated.
+func SetupGated_networkmonitor(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		monitor.SetupGated,
+		probe.SetupGated,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
