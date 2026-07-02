63f405f835bab9a57e2f05f0917623a675e9c949
Erhan Cagirici <erhan@upbound.io>
2026-07-03 02:41:10 +0300

    config: register singleton list paths for new bedrockagentcore resources
    Signed-off-by: Erhan Cagirici <erhan@upbound.io>

--- config/namespaced/bedrockagentcore/config.go
+++ config/namespaced/bedrockagentcore/config.go
@@ -6,6 +6,8 @@ package bedrockagentcore
 
 import (
 	"github.com/crossplane/upjet/v2/pkg/config"
+
+	"github.com/upbound/provider-aws/v2/config/namespaced/common"
 )
 
 func Configure(p *config.Provider) { //nolint:gocyclo
@@ -53,6 +55,17 @@ func Configure(p *config.Provider) { //nolint:gocyclo
 		r.AddSingletonListConversion("certificate[*].location[*].secrets_manager", "certificate[*].location[*].secretsManager")
 		r.AddSingletonListConversion("network_configuration", "networkConfiguration")
 		r.AddSingletonListConversion("network_configuration[*].vpc_config", "networkConfiguration[*].vpcConfig")
+	})
+	// aws_bedrockagentcore_evaluator
+	p.AddResourceConfigurator("aws_bedrockagentcore_evaluator", func(r *config.Resource) {
+		r.AddSingletonListConversion("evaluator_config", "evaluatorConfig")
+		r.AddSingletonListConversion("evaluator_config[*].code_based", "evaluatorConfig[*].codeBased")
+		r.AddSingletonListConversion("evaluator_config[*].code_based[*].lambda_config", "evaluatorConfig[*].codeBased[*].lambdaConfig")
+		r.AddSingletonListConversion("evaluator_config[*].llm_as_a_judge", "evaluatorConfig[*].llmAsAJudge")
+		r.AddSingletonListConversion("evaluator_config[*].llm_as_a_judge[*].model_config", "evaluatorConfig[*].llmAsAJudge[*].modelConfig")
+		r.AddSingletonListConversion("evaluator_config[*].llm_as_a_judge[*].model_config[*].bedrock_evaluator_model_config", "evaluatorConfig[*].llmAsAJudge[*].modelConfig[*].bedrockEvaluatorModelConfig")
+		r.AddSingletonListConversion("evaluator_config[*].llm_as_a_judge[*].model_config[*].bedrock_evaluator_model_config[*].inference_config", "evaluatorConfig[*].llmAsAJudge[*].modelConfig[*].bedrockEvaluatorModelConfig[*].inferenceConfig")
+		r.AddSingletonListConversion("evaluator_config[*].llm_as_a_judge[*].rating_scale", "evaluatorConfig[*].llmAsAJudge[*].ratingScale")
 	})
 	// aws_bedrockagentcore_gateway
 	p.AddResourceConfigurator("aws_bedrockagentcore_gateway", func(r *config.Resource) {
@@ -108,6 +121,36 @@ func Configure(p *config.Provider) { //nolint:gocyclo
 		r.AddSingletonListConversion("target_configuration[*].mcp[*].smithy_model[*].inline_payload", "targetConfiguration[*].mcp[*].smithyModel[*].inlinePayload")
 		r.AddSingletonListConversion("target_configuration[*].mcp[*].smithy_model[*].s3", "targetConfiguration[*].mcp[*].smithyModel[*].s3")
 	})
+	// aws_bedrockagentcore_harness
+	p.AddResourceConfigurator("aws_bedrockagentcore_harness", func(r *config.Resource) {
+		r.References["memory.agentcore_memory_configuration.arn"] = config.Reference{
+			TerraformName: "aws_bedrockagentcore_memory",
+			Extractor:     common.PathARNExtractor,
+		}
+		r.AddSingletonListConversion("authorizer_configuration", "authorizerConfiguration")
+		r.AddSingletonListConversion("authorizer_configuration[*].custom_jwt_authorizer", "authorizerConfiguration[*].customJwtAuthorizer")
+		r.AddSingletonListConversion("authorizer_configuration[*].custom_jwt_authorizer[*].custom_claim[*].authorizing_claim_match_value", "authorizerConfiguration[*].customJwtAuthorizer[*].customClaim[*].authorizingClaimMatchValue")
+		r.AddSingletonListConversion("authorizer_configuration[*].custom_jwt_authorizer[*].custom_claim[*].authorizing_claim_match_value[*].claim_match_value", "authorizerConfiguration[*].customJwtAuthorizer[*].customClaim[*].authorizingClaimMatchValue[*].claimMatchValue")
+		r.AddSingletonListConversion("environment", "environment")
+		r.AddSingletonListConversion("environment_artifact", "environmentArtifact")
+		r.AddSingletonListConversion("environment_artifact[*].container_configuration", "environmentArtifact[*].containerConfiguration")
+		r.AddSingletonListConversion("memory", "memory")
+		r.AddSingletonListConversion("memory[*].agentcore_memory_configuration", "memory[*].agentcoreMemoryConfiguration")
+		r.AddSingletonListConversion("memory[*].agentcore_memory_configuration[*].retrieval_config", "memory[*].agentcoreMemoryConfiguration[*].retrievalConfig")
+		r.AddSingletonListConversion("model", "model")
+		r.AddSingletonListConversion("model[*].bedrock_model_config", "model[*].bedrockModelConfig")
+		r.AddSingletonListConversion("model[*].gemini_model_config", "model[*].geminiModelConfig")
+		r.AddSingletonListConversion("model[*].openai_model_config", "model[*].openaiModelConfig")
+		r.AddSingletonListConversion("tool[*].config", "tool[*].config")
+		r.AddSingletonListConversion("tool[*].config[*].agentcore_browser", "tool[*].config[*].agentcoreBrowser")
+		r.AddSingletonListConversion("tool[*].config[*].agentcore_code_interpreter", "tool[*].config[*].agentcoreCodeInterpreter")
+		r.AddSingletonListConversion("tool[*].config[*].agentcore_gateway", "tool[*].config[*].agentcoreGateway")
+		r.AddSingletonListConversion("tool[*].config[*].agentcore_gateway[*].outbound_auth", "tool[*].config[*].agentcoreGateway[*].outboundAuth")
+		r.AddSingletonListConversion("tool[*].config[*].agentcore_gateway[*].outbound_auth[*].oauth", "tool[*].config[*].agentcoreGateway[*].outboundAuth[*].oauth")
+		r.AddSingletonListConversion("tool[*].config[*].inline_function", "tool[*].config[*].inlineFunction")
+		r.AddSingletonListConversion("tool[*].config[*].remote_mcp", "tool[*].config[*].remoteMcp")
+		r.AddSingletonListConversion("truncation", "truncation")
+	})
 	// aws_bedrockagentcore_memory
 	p.AddResourceConfigurator("aws_bedrockagentcore_memory", func(r *config.Resource) {
 		r.AddSingletonListConversion("stream_delivery_resources", "streamDeliveryResources")
@@ -136,10 +179,29 @@ func Configure(p *config.Provider) { //nolint:gocyclo
 		r.AddSingletonListConversion("oauth2_provider_config[*].salesforce_oauth2_provider_config", "oauth2ProviderConfig[*].salesforceOauth2ProviderConfig")
 		r.AddSingletonListConversion("oauth2_provider_config[*].slack_oauth2_provider_config", "oauth2ProviderConfig[*].slackOauth2ProviderConfig")
 	})
+	// aws_bedrockagentcore_online_evaluation_config
+	p.AddResourceConfigurator("aws_bedrockagentcore_online_evaluation_config", func(r *config.Resource) {
+		r.AddSingletonListConversion("data_source_config", "dataSourceConfig")
+		r.AddSingletonListConversion("data_source_config[*].cloudwatch_logs", "dataSourceConfig[*].cloudwatchLogs")
+		r.AddSingletonListConversion("rule", "rule")
+		r.AddSingletonListConversion("rule[*].session_config", "rule[*].sessionConfig")
+		r.AddSingletonListConversion("rule[*].sampling_config", "rule[*].samplingConfig")
+		r.AddSingletonListConversion("rule[*].filter[*].value", "rule[*].filter[*].value")
+	})
+	// aws_bedrockagentcore_policy
+	p.AddResourceConfigurator("aws_bedrockagentcore_policy", func(r *config.Resource) {
+		r.AddSingletonListConversion("definition", "definition")
+		r.AddSingletonListConversion("definition[*].cedar", "definition[*].cedar")
+	})
+	// aws_bedrockagentcore_resource_policy
+	p.AddResourceConfigurator("aws_bedrockagentcore_resource_policy", func(r *config.Resource) {
+		delete(r.References, "resource_arn")
+	})
 	// aws_bedrockagentcore_token_vault_cmk
 	p.AddResourceConfigurator("aws_bedrockagentcore_token_vault_cmk", func(r *config.Resource) {
 		r.AddSingletonListConversion("kms_configuration", "kmsConfiguration")
 	})
+	// aws_bedrockagentcore_workload_identity
 	p.AddResourceConfigurator("aws_bedrockagentcore_workload_identity", func(r *config.Resource) {
 		r.TerraformResource.Schema["name"].Computed = true
 		r.TerraformResource.Schema["name"].Optional = false
