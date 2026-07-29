e560c5b8d09dbb492e76db1dcd802185dd9477a1
Dave McNeill <dave.mcneill@gmail.com>
2026-07-29 12:47:45 +0100

    account: add PrimaryContact managed resource
    Adds support for aws_account_primary_contact via the account family
    provider. Uses config.IdentifierFromProvider as the external name
    since the resource is a singleton identified by the AWS account ID.
    
    For Observe-only or import scenarios, the crossplane.io/external-name
    annotation must be set to the AWS account ID before applying.
    
    Signed-off-by: Dave McNeill <dave.mcneill@gmail.com>

--- config/externalname.go
+++ config/externalname.go
@@ -267,6 +267,8 @@ var TerraformPluginSDKExternalNameConfigs = map[string]config.ExternalName{
 	//
 	// The Alternate Contact for the current account can be imported using the alternate_contact_type
 	"aws_account_alternate_contact": config.TemplatedStringAsIdentifier("", "{{ .parameters.alternate_contact_type }}"),
+	// The Primary Contact for the current account can be imported using the account_id
+	"aws_account_primary_contact": config.IdentifierFromProvider,
 	// The account region can be imported using region_name or a comma separated account_id and region_name
 	"aws_account_region": config.TemplatedStringAsIdentifier("", "{{ .parameters.region_name }}"),
 
