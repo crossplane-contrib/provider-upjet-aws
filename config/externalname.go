10361d46afbb80ea836fc6f902d9915a6999dc42
Dave McNeill <dave.mcneill@gmail.com>
2026-07-30 09:53:03 +0100

    account: tidy PrimaryContact external-name comment and example annotation
    Signed-off-by: Dave McNeill <dave.mcneill@gmail.com>

--- config/externalname.go
+++ config/externalname.go
@@ -267,7 +267,7 @@ var TerraformPluginSDKExternalNameConfigs = map[string]config.ExternalName{
 	//
 	// The Alternate Contact for the current account can be imported using the alternate_contact_type
 	"aws_account_alternate_contact": config.TemplatedStringAsIdentifier("", "{{ .parameters.alternate_contact_type }}"),
-	// The Primary Contact for the current account can be imported using the account_id
+	// Imported using "default" (current-account) or the explicit account_id.
 	"aws_account_primary_contact": config.IdentifierFromProvider,
 	// The account region can be imported using region_name or a comma separated account_id and region_name
 	"aws_account_region": config.TemplatedStringAsIdentifier("", "{{ .parameters.region_name }}"),
