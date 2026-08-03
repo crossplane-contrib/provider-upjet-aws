b99643dc305d4f106397f8eeaaf4569a813cc8ba
Dave McNeill <dave.mcneill@gmail.com>
2026-08-03 15:06:39 +0100

    account: run make generate for PrimaryContact
    Signed-off-by: Dave McNeill <dave.mcneill@gmail.com>

--- apis/namespaced/account/v1beta1/zz_generated.managed.go
+++ apis/namespaced/account/v1beta1/zz_generated.managed.go
@@ -47,6 +47,46 @@ func (mg *AlternateContact) SetWriteConnectionSecretToReference(r *xpv2.LocalSec
 	mg.Spec.WriteConnectionSecretToReference = r
 }
 
+// GetCondition of this PrimaryContact.
+func (mg *PrimaryContact) GetCondition(ct xpv2.ConditionType) xpv2.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetManagementPolicies of this PrimaryContact.
+func (mg *PrimaryContact) GetManagementPolicies() xpv2.ManagementPolicies {
+	return mg.Spec.ManagementPolicies
+}
+
+// GetProviderConfigReference of this PrimaryContact.
+func (mg *PrimaryContact) GetProviderConfigReference() *xpv2.ProviderConfigReference {
+	return mg.Spec.ProviderConfigReference
+}
+
+// GetWriteConnectionSecretToReference of this PrimaryContact.
+func (mg *PrimaryContact) GetWriteConnectionSecretToReference() *xpv2.LocalSecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this PrimaryContact.
+func (mg *PrimaryContact) SetConditions(c ...xpv2.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetManagementPolicies of this PrimaryContact.
+func (mg *PrimaryContact) SetManagementPolicies(r xpv2.ManagementPolicies) {
+	mg.Spec.ManagementPolicies = r
+}
+
+// SetProviderConfigReference of this PrimaryContact.
+func (mg *PrimaryContact) SetProviderConfigReference(r *xpv2.ProviderConfigReference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+// SetWriteConnectionSecretToReference of this PrimaryContact.
+func (mg *PrimaryContact) SetWriteConnectionSecretToReference(r *xpv2.LocalSecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this Region.
 func (mg *Region) GetCondition(ct xpv2.ConditionType) xpv2.Condition {
 	return mg.Status.GetCondition(ct)
