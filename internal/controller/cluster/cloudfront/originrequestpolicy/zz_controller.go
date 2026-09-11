1c89d15ca23da6cf57e673ec3bd924fd8a3fa0f2
Ott Ramst <ott.ramst@gmail.com>
2026-09-11 13:24:14 +0000

    chore(generate): run make generate for origin request policy change
    Restores name and name_prefix to the OriginRequestPolicy API surface and drops
    the NameAsExternalName initializer from its controllers, following the switch to
    IdentifierFromProvider.
    
    Signed-off-by: Ott Ramst <ott.ramst@gmail.com>

--- internal/controller/cluster/cloudfront/originrequestpolicy/zz_controller.go
+++ internal/controller/cluster/cloudfront/originrequestpolicy/zz_controller.go
@@ -50,7 +50,6 @@ func SetupGated(mgr ctrl.Manager, o tjcontroller.Options) error {
 func Setup(mgr ctrl.Manager, o tjcontroller.Options) error {
 	name := managed.ControllerName(v1beta2.OriginRequestPolicy_GroupVersionKind.String())
 	var initializers managed.InitializerChain
-	initializers = append(initializers, managed.NewNameAsExternalName(mgr.GetClient()))
 	rl := reconciliationpolicy.NewExponentialFailureRateLimiter(time.Second, 60*time.Second)
 	eventHandler := handler.NewEventHandler(handler.WithDefaultRateLimiter(rl), handler.WithLogger(o.Logger.WithValues("gvk", v1beta2.OriginRequestPolicy_GroupVersionKind)))
 	ac := tjcontroller.NewAPICallbacks(mgr, xpresource.ManagedKind(v1beta2.OriginRequestPolicy_GroupVersionKind), tjcontroller.WithEventHandler(eventHandler), tjcontroller.WithStatusUpdates(false))
